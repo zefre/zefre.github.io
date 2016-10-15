a93d64442e987d2cc863705cbea32f72b6eab722
Zefre Affari <zefre@wireless-nat-inside.uiowa.edu>
2016-10-15 13:10:39 -0500

    trying to give gazedot id

--- build/webgazer.js
+++ build/webgazer.js
@@ -10201,6 +10201,7 @@ if (typeof exports !== 'undefined') {
     gazeDot.style.display = 'none';
     gazeDot.style.borderRadius = '100%';
     gazeDot.style.opacity = '0.7';
+    gazeDot.id = "gazedot";
 
     var debugVideoLoc = '';
         
