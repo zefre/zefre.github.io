02a64f396bc39a6fba0c5d39d557b1bb361c8fc5
= <max-salinger@uiowa.edu>
2016-12-01 04:31:54 -0600

    Attempting to add video box

--- demo/scripts/abal-custom.js
+++ demo/scripts/abal-custom.js
@@ -28,7 +28,7 @@ $( document ).ready(function() {
                     //setTimeout(window.speechSynthesis.speak(msg), 10000);
                 }
                 mode = "calibration";
-                $('#videobox').css({'display':'block','margin':'0 auto'});
+                $('#videobox').css({'display':'block','margin':'0 auto','width': '400px','height':'400px'});
                 webgazer.showPredictionPoints(true);
                 $('#gazedot').css('opacity',0.7);
                 hasCalib = true;
