34243750a386529bffebc4c440c1f0c212e50d3c
Jaffar Olegov <jaffarolegov@wireless-nat-inside.uiowa.edu>
2017-03-09 16:22:23 -0600

    Commented speech utterances and shortened success window

--- demo/scripts/abal-custom.js
+++ demo/scripts/abal-custom.js
@@ -8,7 +8,7 @@ $( document ).ready(function() {
             hasCalib = false;
             log = [];
             inBetweenSpeech = 5; //seconds
-            timeOut = 3; //seconds
+            timeOut = 1.5; //seconds
             successCount = 0; //count of # of times in bounds
             loopRun = 0;
             loopSuccessRun = 0;
@@ -135,8 +135,8 @@ $( document ).ready(function() {
                     }
                     if(loopRun % (10*inBetweenSpeech) === 0|| loopRun === 0||!wasInBounds){
                         if(mode !== "calibration"){
-                            var msg = new SpeechSynthesisUtterance('Good');
-                            window.speechSynthesis.speak(msg);
+                            //var msg = new SpeechSynthesisUtterance('Good');
+                            //window.speechSynthesis.speak(msg);
                         }
                         wasInBounds = true;
                     }
@@ -147,8 +147,8 @@ $( document ).ready(function() {
                     if(loopRun % (10*inBetweenSpeech) === 0|| loopRun === 0||wasInBounds){
                         if(mode !== "calibration"){
                             loopSuccessRun = 0;
-                            var msg = new SpeechSynthesisUtterance('Make eye contact');
-                            window.speechSynthesis.speak(msg);
+                            //var msg = new SpeechSynthesisUtterance('Make eye contact');
+                            //window.speechSynthesis.speak(msg);
                         }
                     }
                     wasInBounds = false;
